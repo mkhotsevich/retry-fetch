8de0ec24183262e649f4d973660f8462db0b4874
mvkhotsevich <mvkhotsevich@avito.ru>
2024-06-26 15:59:07 +0300

    fetch image

--- src/index.js
+++ src/index.js
@@ -1,85 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3000;
+
+function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Retry Fetch Example</title>
-    </head>
-    <body>
-      <h1>Retry Fetch Example</h1>
-      <label for="attempts">Number of Attempts:</label>
-      <input type="number" id="attempts" value="3">
-      <br><br>
-      <button onclick="makeRequest('/status200', 'GET')">Request /status200</button>
-      <button onclick="makeRequest('/status500', 'GET')">Request /status500</button>
-      <button onclick="makeRequest('/status500', 'PUT')">Request /status500 (PUT)</button>
-      <button onclick="makeRequest('/status401', 'GET')">Request /status401</button>
+    const imagePath = path.join(__dirname, 'templates', 'image.html');
+    const retryFetchPath = path.join(__dirname, 'templates', 'retryFetch.html');
 
-      <script>
-        function retryFetch(url, options, attempts = 0) {
-            return fetch(url, options)
-                .then((response) => {
-                    if (response.status === 401 || response.status === 403) {
-                        throw response;
-                    }
-        
-                    if (response.status >= 400) {
-                        if (attempts > 0 && options.method !== 'PUT') {
-                            return retryFetch(url, options, attempts - 1, response);
-                        } else {
-                            throw response;
-                        }
-                    }
-        
-                    return response;            
-                })
-                .catch((error) => {
-                    if (attempts > 0 && options.method !== 'PUT') {
-                        return retryFetch(url, options, attempts - 1);
-                    }
-        
-                    throw error;
-                });
-        }
-
-        function makeRequest(endpoint, method) {
-          const attempts = parseInt(document.getElementById('attempts').value);
-          const options = {
-            method: method,
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          };
-
-          retryFetch(endpoint, options, attempts)
-            .then(response => response.text())
-            .then(data => {
-              alert('Response: ' + data);
-            })
-            .catch(error => {
-              alert('Error: ' + error.statusText);
-            });
-        }
-      </script>
-    </body>
-    </html>
-  `);
+    res.sendFile(imagePath);
 });
-
 
 app.get('/status200', (req, res) => {
     res.status(200).send('Status 200: OK');
 });
 
+
+app.put('/status200', (req, res) => {
+    res.status(200).send('Status 200: OK (PUT)');
+});
+
+app.get('/status300', (req, res) => {
+    res.status(300).send('Status 300: OK');
+});
 
 app.get('/status500', (req, res) => {
     res.status(500).send('Status 500: Internal Server Error');
@@ -92,6 +42,17 @@ app.put('/status500', (req, res) => {
 app.get('/status401', (req, res) => {
     res.status(401).send('Status 401: Unauthorized');
 });
+
+app.get('/status403', (req, res) => {
+    res.status(403).send('Status 403: Forbidenn');
+});
+
+app.get('/image', (req, res) => {
+    const imagePath = path.join(__dirname, 'images', `${randomIntFromInterval(1, 3)}.png`);
+
+    res.sendFile(imagePath);
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
